dc1be1597ec3183861c521cb867052ac82c45187
samlehoy <muttaqien0111@gmail.com>
2023-11-26 16:54:51 +0700

    UPDATE: 1. Fix load fragment crash 2. Fix warna hijau disekitar bottom nav
    BUG:
    1. RecyclerView data dokter tidak muncul

--- app/src/main/java/com/example/codoc/HomeActivity.kt
+++ app/src/main/java/com/example/codoc/HomeActivity.kt
@@ -3,9 +3,12 @@ package com.example.codoc
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
 
@@ -13,10 +16,37 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val homenav : FrameLayout = findViewById (R.id.homenav)
-        homenav.setOnClickListener {
-            val intent = Intent (this, FragmentHome::class.java)
-            startActivity(intent)
+        //hide title bar
+        getSupportActionBar()?.hide()
+
+        //instance
+        val bottomNav:BottomNavigationView = findViewById(R.id.bottomNav);
+
+        //set fragment
+        val homeFragment=FragmentHome()
+
+        //default fragment
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container,homeFragment)
+            commit()
+        }
+
+        currentFragment(homeFragment)
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home->currentFragment(homeFragment)
+            }
+            true
         }
     }
+
+    private fun currentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container,fragment)
+            commit()
+        }
+
 }
+
+
