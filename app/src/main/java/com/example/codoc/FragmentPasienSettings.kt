e24942e82c1bc32dceb67f573592d53f29c4a3dc
samlehoy <muttaqien0111@gmail.com>
2023-12-21 21:26:43 +0700

    UPDATE: 21 Desember 2023 1. getDataJanjiPasien & saveAppointment Selesai 2. activity_booking SELESAI 3. activity_myjanji PASIEN DAN DOKTER SELESAI 4. SettingsActivity -> (1. About Us, 2.Need Help, 3.Feedback)
    BUG:
    1. Register pasien dan dokter ada bug ketika data null tetap bisa teregister
    
    KEKURANGAN:

--- app/src/main/java/com/example/codoc/FragmentPasienSettings.kt
+++ app/src/main/java/com/example/codoc/FragmentPasienSettings.kt
@@ -1,12 +1,14 @@
 package com.example.codoc
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.appcompat.app.AlertDialog
 import com.example.codoc.activity.FirstActivity
 import com.example.codoc.activity.pasien.ProfilePasienActivity
 
@@ -39,6 +41,17 @@ class FragmentPasienSettings : Fragment() {
         val view = inflater.inflate(R.layout.fragment_pasien_settings, container, false)
 
         // Find the LinearLayout using the inflated view
+        val aboutUs: LinearLayout = view.findViewById(R.id.linearLayout2)
+        aboutUs.setOnClickListener {
+            showAboutUsDialog()
+        }
+
+        // Find the LinearLayout using the inflated view
+        val feedback: LinearLayout = view.findViewById(R.id.linearLayout4)
+        feedback.setOnClickListener {
+            sendFeedbackEmail()
+        }
+        // Find the LinearLayout using the inflated view
         val editProfile: LinearLayout = view.findViewById(R.id.linearLayout1)
         editProfile.setOnClickListener {
             val intentSettingActivity = Intent(requireContext(), ProfilePasienActivity::class.java)
@@ -52,6 +65,55 @@ class FragmentPasienSettings : Fragment() {
         }
 
         return view
+    }
+
+    private fun sendFeedbackEmail() {
+        // Create an implicit intent to send an email
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.data = Uri.parse("mailto:muttaqien0111@gmail.com") // Set the email address
+
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Feedback") // Set the email subject
+
+        // You can customize the email body if needed
+        intent.putExtra(
+            Intent.EXTRA_TEXT,
+            "Dear Team,\n\nI would like to provide the following feedback:\n\n"
+        )
+
+        // Check if there's an activity that can handle the email intent
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivity(intent)
+        } else {
+            // Handle the case where no email app is available
+            // You can display a Toast or show an AlertDialog
+            // indicating that no email app is installed.
+        }
+    }
+
+    private fun showAboutUsDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("About Us")
+            .setMessage("Kami adalah tim pengembang yang berkomitmen untuk menyediakan pengalaman pengguna terbaik dalam pendaftaran janji dokter. " +
+                    "Aplikasi CRUD (Create, Read, Update, Delete) kami diciptakan dengan tujuan untuk memberikan solusi yang ramah pengguna, cepat, dan dapat diandalkan bagi pengguna yang ingin membuat janji dengan dokter mereka.")
+            .setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .setNeutralButton("Learn More") { _, _ ->
+                // Create an implicit intent to open the URL
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/samlehoy/Codoc-Project-Appointment-App"))
+
+                // Check if there's an activity that can handle the intent
+                if (intent.resolveActivity(requireActivity().packageManager) != null) {
+                    startActivity(intent)
+                } else {
+                    // Handle the case where no app is available to handle the intent
+                    // You can display a Toast or show an AlertDialog
+                    // indicating that no app is available.
+                }
+            }
+
+        val dialog = builder.create()
+        dialog.show()
     }
 
     companion object {
